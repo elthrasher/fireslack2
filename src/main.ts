2df9d88bf2736ed41395aaab89d49460e9d6b8e7
Matt Martz <matt.martz@gmail.com>
2016-05-07 13:06:18 -0400

    firebase.ts needed to be in app

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { bootstrap } from '@angular/platform-browser-dynamic';
 import { ComponentRef, enableProdMode, provide } from '@angular/core';
 import { ROUTER_PROVIDERS } from '@angular/router';
 import { Fireslack2AppComponent, environment } from './app/';
-// import { FIREBASE_APP_PROVIDERS } from './firebase';
+import { FIREBASE_APP_PROVIDERS } from './app/firebase';
 import { AuthService, AuthRouteHelperService, AUTH_PROVIDERS } from './app/shared';
 
 if (environment.production) {
@@ -11,7 +11,7 @@ if (environment.production) {
 
 const providers: any[] = [
   AUTH_PROVIDERS,
-  // FIREBASE_APP_PROVIDERS,
+  FIREBASE_APP_PROVIDERS,
   ROUTER_PROVIDERS
   //, provide(APP_BASE_HREF, {useValue: '/'})
 ];
